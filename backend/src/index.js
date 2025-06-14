f476e42f09b6bfed0901e835b7e081256c45b615
amiz-seoat <abduselammiz6@gmail.com>
2025-06-14 09:14:39 -0400

    finish update profile functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,15 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser"
 
 const app = express()
-dotenv.config()
+dotenv.config() //try to figure out the importance of this line.
 
 const PORT = process.env.PORT
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 
