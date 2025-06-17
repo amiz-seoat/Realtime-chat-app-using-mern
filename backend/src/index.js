858c4aa219ed01f2c6ce259c222180ae21b68fa2
amiz-seoat <abduselammiz6@gmail.com>
2025-06-17 10:52:31 -0400

    finishing message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
+import messageRoutes from "./routes/message.route.js"
 
 const app = express()
 dotenv.config() //try to figure out the importance of this line.
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, () => {
     console.log("server is running on port 5001")
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+import daisyui from "daisyui"
+
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // adjust if your project structure is different
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [daisyui],
+    
+  }
