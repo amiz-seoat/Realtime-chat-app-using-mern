b4fc8b23fbfbe2c530d85e4dfdd8169412a3d2d5
amiz-seoat <abduselammiz6@gmail.com>
2025-06-19 05:28:15 -0400

    finishing signup page ui with its functionality and also logout functionality

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,9 +1,10 @@
 import { create } from "zustand"
 import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
     authUser: null,
-    isSigningUP: false,
+    isSigningUp: false,
     isLoggingUp: false,
     isUpdatingProfile: false,
  
@@ -20,5 +21,32 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({ isCheckingAuth: false })
         }
-    }
+    },
+
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+          const res = await axiosInstance.post("/auth/signup", data);
+          set({ authUser: res.data });
+          toast.success("Account created successfully");
+        } catch (error) {
+          console.error("Signup error:", error);
+          const message =
+            error?.response?.data?.message || "Something went wrong during signup";
+          toast.error(message);
+        } finally {
+          set({ isSigningUp: false });
+        }
+      },
+      
+  logout: async () => {
+      try {
+        await axiosInstance.post("auth/logout")
+        set({ authUser: null })
+        toast.success("Logged out successfully")
+      } catch (error) {
+        toast.error(error.response.data.message)
+      }
+    },
+    
 }));
